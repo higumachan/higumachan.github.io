913da6126ac5bb4b549f5d2127127d350e0639ea
Yuta Hinokuma <yuta@abeja.asia>
2015-03-17 19:09:51 +0900

    test

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-KinectDepthRecordSystem -c C:\Users\daikin\Desktop\settings.json
+C:\Users\daikin\Desktop\KinectDepthRecorder\Release\KinectDepthRecordSystem -c C:\Users\daikin\Desktop\settings.json
